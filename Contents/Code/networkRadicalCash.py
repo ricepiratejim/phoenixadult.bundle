d5fce62a1716c34d544192f9384557243aed7331
diamondpete <87245367+diamondpete@users.noreply.github.com>
2024-08-01 01:29:39 -0400

    Refactor/Move Radical Cash Sites to API Search and Add Sites

--- Contents/Code/networkRadicalCash.py
+++ Contents/Code/networkRadicalCash.py
@@ -9,43 +9,49 @@ def search(results, lang, siteNum, searchData):
         sceneId = parts[0]
         searchData.title = searchData.title.replace(sceneId, '', 1).strip()
 
-    if PAsearchSites.getSearchSiteName(siteNum) == 'ComeInside':
-        searchURL = PAsearchSites.getSearchSearchURL(siteNum) + slugify(searchData.title.lower())
-    else:
-        searchURL = PAsearchSites.getSearchSearchURL(siteNum) + searchData.encoded.lower()
-
+    searchURL = '%s/api/search/%s' % (PAsearchSites.getSearchBaseURL(siteNum), searchData.encoded.lower())
     req = PAutils.HTTPRequest(searchURL)
-    searchPageElements = HTML.ElementFromString(req.text)
-
-    searchResults = json.loads(searchPageElements.xpath('//script[@type="application/json"]')[0].text_content())
-
-    if PAsearchSites.getSearchSiteName(siteNum) == 'ComeInside':
-        data = searchResults['props']['pageProps']['model_contents']
-    else:
-        data = searchResults['props']['pageProps']['contents']['data']
+    searchResults = req.json()
+    data = searchResults['playlists'] if siteNum == 1677 else searchResults['scenes']
 
     if searchResults:
         for searchResult in data:
-            titleNoFormatting = PAutils.parseTitle(searchResult['title'], siteNum)
-            videoId = searchResult['id']
+            for type in ['scene', 'bts']:
+                if type == 'bts' and siteNum == 1835:
+                    titleNoFormatting = 'BTS: %s' % PAutils.parseTitle(searchResult['title'], siteNum)
 
-            if PAsearchSites.getSearchSiteName(siteNum) == 'ComeInside':
-                releaseDate = parse(searchResult['created_at']).strftime('%Y-%m-%d')
-                sceneURL = '%s/scenes/%s/%s' % (PAsearchSites.getSearchBaseURL(siteNum), videoId, searchResult['slug'])
-            else:
-                releaseDate = parse(searchResult['publish_date']).strftime('%Y-%m-%d')
-                sceneURL = '%s/videos/%s' % (PAsearchSites.getSearchBaseURL(siteNum), searchResult['slug'])
+                    releaseDate = parse(searchResult['publish_date']).strftime('%Y-%m-%d')
+                    sceneURL = '%s/bts/%s-bts' % (PAsearchSites.getSearchBaseURL(siteNum), searchResult['slug'])
 
-            curID = PAutils.Encode(sceneURL)
+                    curID = PAutils.Encode(sceneURL)
 
-            if sceneId and int(sceneId) == int(videoId):
-                score == 100
-            elif searchData.date:
-                score = 80 - Util.LevenshteinDistance(searchData.date, releaseDate)
-            else:
-                score = 80 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
+                    if searchData.date:
+                        score = 80 - Util.LevenshteinDistance(searchData.date, releaseDate)
+                    else:
+                        score = 80 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
 
-            results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [%s] %s' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum), releaseDate), score=score, lang=lang))
+                    results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [%s] %s' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum), releaseDate), score=score, lang=lang))
+                elif type == 'scene':
+                    titleNoFormatting = PAutils.parseTitle(searchResult['title'], siteNum)
+                    videoId = searchResult['id']
+
+                    if siteNum == 1677:
+                        releaseDate = parse(searchResult['created_at']).strftime('%Y-%m-%d')
+                        sceneURL = '%s/%s/%s' % (PAsearchSites.getSearchSearchURL(siteNum), videoId, searchResult['slug'])
+                    else:
+                        releaseDate = parse(searchResult['publish_date']).strftime('%Y-%m-%d')
+                        sceneURL = '%s/%s' % (PAsearchSites.getSearchSearchURL(siteNum), searchResult['slug'])
+
+                    curID = PAutils.Encode(sceneURL)
+
+                    if sceneId and int(sceneId) == int(videoId):
+                        score == 100
+                    elif searchData.date:
+                        score = 80 - Util.LevenshteinDistance(searchData.date, releaseDate)
+                    else:
+                        score = 80 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
+
+                    results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [%s] %s' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum), releaseDate), score=score, lang=lang))
 
     return results
 
@@ -58,7 +64,7 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
     detailsPageElements = HTML.ElementFromString(req.text)
     videoPageElements = json.loads(detailsPageElements.xpath('//script[@type="application/json"]')[0].text_content())
 
-    if PAsearchSites.getSearchSiteName(siteNum) == 'ComeInside':
+    if siteNum == 1677:
         video = videoPageElements['props']['pageProps']['playlist']
         content = videoPageElements['props']['pageProps']['content']
     else:
@@ -69,14 +75,23 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
     metadata.title = PAutils.parseTitle(video['title'], siteNum)
 
     # Summary
-    metadata.summary = video['description']
+    summary = video['description']
+    if not re.search(r'.$(?<=(!|\.|\?))', summary):
+        summary = summary + '.'
+    metadata.summary = summary
 
     # Studio
-    metadata.studio = 'Radical Cash'
+    if (1229 <= siteNum <= 1236):
+        metadata.studio = 'Top Web Models'
+    elif (837 <= siteNum <= 839):
+        metadata.studio = 'TwoWebMedia'
+    else:
+        metadata.studio = 'Radical Cash'
 
     # Tagline and Collection(s)
-    tagline = PAsearchSites.getSearchSiteName(siteNum)
-    metadata.tagline = tagline
+    tagline = content['site'] if siteNum == 1677 else video['site']
+    if metadata.studio != tagline:
+        metadata.tagline = tagline
     metadata.collections.add(tagline)
 
     # Release Date
@@ -98,12 +113,20 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
         movieActors.addActor(actorName, actorPhotoURL)
 
     # Posters
-    art.append(content['trailer_screencap'])
+    if content['trailer_screencap']:
+        art.append(content['trailer_screencap'])
+
+    if 'previews' in content:
+        for image in content['previews']['full']:
+            art.append(image)
+
     for imageType in ['extra_thumbnails']:
         if imageType in content:
             for image in list(content[imageType]):
                 art.append(image)
 
+    images = []
+    posterExists = False
     Log('Artwork found: %d' % len(art))
     for idx, posterUrl in enumerate(art, 1):
         if not PAsearchSites.posterAlreadyExists(posterUrl, metadata):
@@ -117,9 +140,26 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
                 if height > width:
                     # Item is a poster
                     metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+                    posterExists = True
                 if width > height:
                     # Item is an art item
+                    images.append((image, posterUrl))
                     metadata.art[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+            except:
+                pass
+        elif PAsearchSites.posterOnlyAlreadyExists(posterUrl, metadata):
+            posterExists = True
+
+    if not posterExists:
+        for idx, (image, posterUrl) in enumerate(images, 1):
+            try:
+                im = StringIO(image.content)
+                resized_image = Image.open(im)
+                width, height = resized_image.size
+                # Add the image proxy items to the collection
+                if width > 1:
+                    # Item is a poster
+                    metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
             except:
                 pass
 
