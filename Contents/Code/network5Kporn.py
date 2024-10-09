caba6eaa043c8a1eb14cb60cddb361f9203249bb
diamondpete <87245367+diamondpete@users.noreply.github.com>
2024-10-09 18:07:16 -0400

    Improve 5Kporn Image and Collection Handling

--- Contents/Code/network5Kporn.py
+++ Contents/Code/network5Kporn.py
@@ -41,7 +41,11 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
     metadata.studio = '5Kporn'
 
     # Tagline and Collection(s)
-    metadata.tagline = PAsearchSites.getSearchSiteName(siteNum)
+    if '5KT' in sceneURL:
+        tagline = '5Kteens'
+    else:
+        tagline = PAsearchSites.getSearchSiteName(siteNum)
+    metadata.tagline = tagline
     metadata.collections.add(metadata.tagline)
 
     # Date
@@ -93,11 +97,12 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
 
     Log('Artwork found: %d' % len(art))
     images = []
+    postersClean = list()
     posterExists = False
     for idx, posterUrl in enumerate(art, 1):
         # Remove Timestamp and Token from URL
         cleanUrl = re.sub(r'\/expiretime=.*?(?<=\/).*?(?=\/)', '', posterUrl.split('?')[0])
-        art[idx - 1] = cleanUrl
+        postersClean.append(cleanUrl)
         if not PAsearchSites.posterAlreadyExists(cleanUrl, metadata):
             # Download image file for analysis
             try:
@@ -131,5 +136,7 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, art):
                     metadata.posters[cleanUrl] = Proxy.Media(image.content, sort_order=idx)
             except:
                 pass
+
+    art.extend(postersClean)
 
     return metadata
